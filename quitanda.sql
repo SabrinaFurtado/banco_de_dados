7551e00ceac30f62e7985a0f8425b864231e7154
Sabrina Furtado <145471677+SabrinaFurtado@users.noreply.github.com>
2024-03-05 11:24:56 -0300

    Delete quitanda.sql

--- quitanda.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE td_categorias (
-id bigint auto_increment primary key,
-descricao VARCHAR(255) NOT NULL);
-
-SELECT * FROM db_quitanda, td_categorias;
-
-INSERT INTO td_categorias (descricao)
-VALUES ("Fruta"),("Verduras"), ("Legumes"), ("Temperos"), ("Ovos"), ("Outros");
